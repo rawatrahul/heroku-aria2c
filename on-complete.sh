a67587391d66edbef09bd0a95bc4dfa2c4326938
Rahul Rawat <rahulrawat593@gmail.com>
2020-01-09 13:48:46 -0700

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [[ -n $3 ]]; then
 	echo "aria2 parameters: $1 $2 $3"
-	rclone -v --config="rclone.conf" copy "$3" "heroku:$RCLONE_DESTINATION" 2>&1
+	rclone -v --config="rclone.conf" copy "downloads" "heroku:$RCLONE_DESTINATION" 2>&1
 fi
