171bed50efcf05355b21facceabe2e26cfaf4b74
Rahul Rawat <rahulrawat593@gmail.com>
2020-01-09 10:28:34 -0700

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+k#!/bin/bash
 if [[ -n $3 ]]; then
-	rclone -v --config="rclone.conf" copy "$3" "DRIVE:$RCLONE_DESTINATION" 2>&1
+	rclone -v --config="rclone.conf" copy "$3" "heroku:$RCLONE_DESTINATION" 2>&1
 fi
