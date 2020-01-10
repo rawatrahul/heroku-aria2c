4236b741c35e6ab12aeb6a69dfa192b8c7143d8f
Rahul Rawat <rahulrawat593@gmail.com>
2020-01-09 22:59:17 -0700

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [[ -n $3 ]]; then
 	echo "aria2 parameters: $1 $2 $(dirname "$3")"
-	rclone -v --config="rclone.conf" move "$(dirname "$3")" "heroku:$RCLONE_DESTINATION/"$(basename "$(dirname "$3")")""  --exclude="*.aria2"  --exclude="*.exe"  --exclude="*.nfo"  --exclude="*.txt"  2>&1
+	rclone -v --config="rclone.conf" move "$(dirname "$3")" "heroku:$RCLONE_DESTINATION"$(basename "$(dirname "$3")")""  --exclude="*.aria2"  --exclude="*.exe"  --exclude="*.nfo"  --exclude="*.txt"  2>&1
 fi
